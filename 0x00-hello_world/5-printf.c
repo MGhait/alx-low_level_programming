cf719c73e0ebd818b0e447560afb16f89e8cd7ee
Mohamed-VM_Vagrant <mosaad05019@gmail.com>
2023-03-17 13:14:59 +0000

    editing file 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: A C program taht prints using printf functio
+ *
+ * Return: Alawys o (Success)
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
