67fd008434f7d3175886d403653d19405d1b3bcf
Mohamed-VM_Vagrant <mosaad05019@gmail.com>
2023-03-18 00:37:00 +0000

    adding file 5-print_numbers.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -4,7 +4,10 @@
 
 /**
  * main - Entry point
- * Retrun: Always 0 (Success)
+ *
+ * Description: prints the alphabet in lowercase, followed by a new line
+ *
+ * Return: Always 0 (Success)
 */
 
 int main(void)
