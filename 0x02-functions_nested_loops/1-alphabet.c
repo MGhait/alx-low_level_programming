0df5370624893a13a45ca8437fbc9cb719196f62
Mohamed-VM_Vagrant <mosaad05019@gmail.com>
2023-03-18 01:57:19 +0000

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alpthaber - rint all alphabet in lowercase
+*/
+
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+
+	_putchar('\n');
+}
