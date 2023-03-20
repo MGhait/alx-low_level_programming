d3bb1e8596ff4f59ad4b02d030d1de4e75c11e27
Mohamed-VM_Vagrant <mosaad05019@gmail.com>
2023-03-20 19:33:00 +0000

    a

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -8,12 +8,12 @@
  *
  * Return: @dest
  */
-char *strncpy(char *dest, char *src, int n)
+char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
-		dest[i];
+		dest[i] = src[i];
 
 	while (i < n)
 	{
