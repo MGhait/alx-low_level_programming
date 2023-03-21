634502806759a2e694a36e2aac2a928a2e6e7812
Mohamed-VM_Vagrant <mosaad05019@gmail.com>
2023-03-21 06:07:03 +0000

    adding file

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,3 +1,27 @@
 #include "main.h"
 /**
- * print_chessboard*/
+ * _strstr - Entry point
+ * @haystack: input
+ * @needle: iput
+ * Return: on success 0
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *l = haystack;
+		char *p = needle;
+
+		while (*l == *p && *p != '\0')
+		{
+			l++;
+			p++;
+		}
+
+		if (*p == '\0')
+			return (haystack);
+	}
+
+	return (0);
+}
