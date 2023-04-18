f0699524818cafa3969a365aa46e8163e28cffb4
Mohamed-VM_Vagrant <mosaad05019@gmail.com>
2023-04-18 22:03:14 +0000

    a

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,7 +7,7 @@
  * @age: dog's age
  * @owner: owener's name
  *
- * Description: jast a lone dog struct in a big kitty world
+ * Description: jast a description
  */
 
 struct dog
@@ -17,9 +17,18 @@ struct dog
 	char *owner;
 };
 
+/**
+ * dog_t typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int_strlen(char *s);
+
 
 #endif
