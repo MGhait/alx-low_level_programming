a8d2defc420ff427bb75a486faaf0839757e30ce
Mohamed Saad <mosaad05019@gmail.com>
2023-04-28 19:53:46 +0000

    a

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -20,8 +20,8 @@ typedef struct list_s
 
 int _putchar(char c);
 int _strlen(char *s);
-size_t print_list(const list_t *h);
-size_t list_len(const list_t *h);
+size_t print_listint(const listint_t *h);
+size_t listint_len(const listint_t *h);
 list_t *add_node(list_t **head, const int n);
 list_t *add_node_end(list_t **head, const int n);
 void free_list(list_t *head);
