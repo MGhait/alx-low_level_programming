f70c850a48e5f553bc2f539340ffd734be0cc1e7
Mohamed Saad <mosaad05019@gmail.com>
2023-04-28 02:29:46 +0000

    a

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,19 +1,37 @@
 #include "lists.h"
 
 /**
- * listint_len - returns the number of elements in a linked lists
+ * _strlen - returns the length of a string
+ * @s: the string
+ * Return: interger length
+ */
+int _strlen(char *s)
+{
+        int i = 0;
+
+        if (!s)
+                return (0);
+        while (*s++)
+                i++;
+        return (i);
+}
+
+
+/**
+ * print_list - returns the number of elements in a linked lists
  * @h: linked list of type listint_t to traverse
  *
  * Return: number of nodes
  */
-size_t listint_len(const listint_t *h)
+size_t print_list(const list_t *h)
 {
 	size_t num = 0;
 
 	while (h)
 	{
-		num++;
+		printf("[%d] %s\n", _strlen(h->str), h->str ? h->str : "(nil)");
 		h = h->next;
+		num++;
 	}
 
 	return (num);
