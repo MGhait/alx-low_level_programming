9ce7779cb3649737c58d7174fefbc00825c61845
Mohamed Saad <mosaad05019@gmail.com>
2023-09-29 19:58:53 +0000

    0-puts-recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts_recursion - retern string followed by new line
+ * @s: string
+ * Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+}
+
+
