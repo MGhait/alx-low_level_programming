f39f00957442a02e5cb80d05e6244aa5cb3df5b8
Mohamed Saad <mosaad05019@gmail.com>
2023-10-09 17:59:45 +0000

    push

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen - teturns the length of a string
+ *
+ * @s: string parameter input
+ *
+ * Retrun: length of string
+ */
+
+int _strlen(char *s)
+{
+	int counter;
+
+	for (counter = 0; *s != '\0'; s++)
+		++counter;
+
+	return (counter);
+}
